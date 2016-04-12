47db6555582c3e3ec960948077fa510a333baa04
Williamallas <jerome.torche@epfl.ch>
2016-04-12 18:07:23 +0200

    Added helper function genParam

--- Source/TIMEngine2/interface/Texture.h
+++ Source/TIMEngine2/interface/Texture.h
@@ -28,6 +28,21 @@ namespace interface
     public:
         using resource::Asset<renderer::Texture>::Asset;
 
+        static renderer::Texture::GenTexParam genParam(bool repeat=true, bool linear=true, bool trilinear=true, int aniso=0)
+        {
+            renderer::Texture::GenTexParam param;
+            param.anisotropy = aniso;
+            param.depthMode = false;
+            param.format = renderer::Texture::Format::RGBA8;
+            param.linear = linear;
+            param.nbLevels = -1;
+            param.repeat = repeat;
+            param.trilinear = trilinear;
+            return param;
+        }
+
+        uint64_t handle() const { return _ptr->handle(); }
+
     protected:
         renderer::Texture* texture() const { return _ptr.get(); }
     };
