c23fe23c6d2be2eadfba663e8cb82bfb3a1ee145
Williamallas <jerome.torche@epfl.ch>
2016-04-16 20:31:44 +0200

    bullet Height map supported

---
+++ Source/TIMEngine2/bullet/HeightFieldShape.h
@@ -0,0 +1,27 @@
+#ifndef HEIGHTFIELDSHAPE_H
+#define HEIGHTFIELDSHAPE_H
+
+#include "core.h"
+
+#include "btBulletCollisionCommon.h"
+#include "ImageAlgorithm.h"
+
+namespace tim
+{
+
+static btHeightfieldTerrainShape* createHeightFieldShape(vec3 size, const ImageAlgorithm<float>& img)
+{
+    size /= vec3(img.size().x()-1, img.size().y()-1,1);
+    btHeightfieldTerrainShape* shape = new btHeightfieldTerrainShape(img.size().x(), img.size().y(),
+                                                                     img.data(),
+                                                                     1,
+                                                                     2, // z up
+                                                                     true, // float data
+                                                                     false);
+    shape->setLocalScaling(btVector3(size.x(), size.y(), size.z()));
+    return shape;
+}
+
+}
+
+#endif // HEIGHTFIELDSHAPE_H
