599baef782fd91c5892c2fbb8d46bea5245c975f
Williamallas <jerome.torche@epfl.ch>
2016-08-24 02:21:09 +0200

    new sound asset loader

--- Source/TIMEngine2/resource/AddYourLoader.h
+++ Source/TIMEngine2/resource/AddYourLoader.h
@@ -5,4 +5,38 @@
 #include "interface/GeometryAssetLoader.h"
 #include "interface/TextureAssetLoader.h"
 
+#ifndef NO_OPEN_AL
+#include "SoundAsset.h"
+namespace tim {
+    using namespace core;
+namespace resource {
+
+    template <>
+    class AssetLoader<SoundAsset>
+    {
+    public:
+        template<bool async>
+        Option<SoundAsset> operator()(std::string file, bool stream, Sampler::Conversion conv = Sampler::LEFT)
+        {
+            static_assert(!async, "Async soundasset loading not supported");
+
+            Sampler* sampler = new Sampler;
+            bool ok = sampler->load(file, conv);
+            if(ok)
+            {
+                Sound * sound = new Sound(sampler, stream);
+                return Option<SoundAsset>(sound);
+            }
+            else
+            {
+                delete sampler;
+                return Option<SoundAsset>();
+            }
+        }
+    };
+
+}
+}
+#endif
+
 #endif // ADDYOURLOADER_H_INCLUDED
