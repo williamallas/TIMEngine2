f326ba53e29bf58b237f2245b442771998394bec
Williamallas <jerome.torche@epfl.ch>
2016-08-24 02:21:25 +0200

    pooled texture/fbo

--- Source/TIMEngine2/renderer/AbstractRenderer.h
+++ Source/TIMEngine2/renderer/AbstractRenderer.h
@@ -2,7 +2,8 @@
 #define ABSTRACTRENDERER_H
 
 #include "core/core.h"
-#include "FrameBuffer.h"
+#include "renderer.h"
+#include "PooledBuffer.h"
 #include "FrameParameter.h"
 
 #include "MemoryLoggerOn.h"
@@ -15,17 +16,20 @@ namespace renderer
     class AbstractRenderer : boost::noncopyable
     {
     public:
-        AbstractRenderer(const uivec2& res, const FrameParameter& param) : _frameState(param), _resolution(res), _fbo(res) {}
+        AbstractRenderer(const uivec2& res, const FrameParameter& param) : _frameState(param), _resolution(res), _buffer(renderer::texBufferPool) {}
         virtual ~AbstractRenderer() = default;
 
         const uivec2& resolution() const { return _resolution; }
-        const FrameBuffer& frameBuffer() const { return _fbo; }
+        const FrameBuffer* frameBuffer() const { return _buffer.fbo(); }
         const FrameParameter& frameState() const { return _frameState; }
+
+        void acquire() { _buffer.acquire(); }
+        void release() { _buffer.release(); }
 
     protected:
         const FrameParameter& _frameState;
         uivec2 _resolution;
-        FrameBuffer _fbo;
+        PooledBuffer _buffer;
     };
 
 }
