077ab784496505aa980e61663ff74e07616c149d
Williamallas <jerome.torche@epfl.ch>
2016-08-24 02:19:16 +0200

    new VRDebugCamera for room scale emulation

--- Source/OpenVR/HmdSceneView.h
+++ Source/OpenVR/HmdSceneView.h
@@ -3,6 +3,7 @@
 
 #include "interface/pipeline/pipeline.h"
 #include "OpenVR/VR_Device.h"
+#include "OpenVR/VRDebugCamera.h"
 
 namespace tim {
     struct HmdSceneView
@@ -31,7 +32,7 @@ namespace tim {
 
         void addOffset(const mat4& o)
         {
-            _offset *= o;
+            _offset = o * _offset;
         }
 
         const mat4& offset() const { return _offset; }
@@ -45,45 +46,86 @@ namespace tim {
             _eyeView[VR_Device::RIGHT].camera.useRawMat = true;
 
             mat4 inv_o = _offset.inverted();
+            _transform = hmdDevice.camera().hmdView() * inv_o;
 
             _eyeView[VR_Device::LEFT].camera.raw_proj = hmdDevice.camera().eyeProjection(VR_Device::LEFT);
             _eyeView[VR_Device::LEFT].camera.raw_view = hmdDevice.camera().eyeView(VR_Device::LEFT) * inv_o;
             _eyeView[VR_Device::RIGHT].camera.raw_proj = hmdDevice.camera().eyeProjection(VR_Device::RIGHT);
             _eyeView[VR_Device::RIGHT].camera.raw_view = hmdDevice.camera().eyeView(VR_Device::RIGHT) * inv_o;
 
-            _cullingView.camera.pos = _offset * hmdDevice.camera().hmdView().inverted().translation();
+            mat4 inv_l = _eyeView[VR_Device::LEFT].camera.raw_view.inverted();
+            mat4 inv_r = _eyeView[VR_Device::RIGHT].camera.raw_view.inverted();
 
-            _eyeView[VR_Device::RIGHT].camera.pos = _eyeView[VR_Device::RIGHT].camera.raw_view.inverted().translation();
-            _eyeView[VR_Device::LEFT].camera.pos = _eyeView[VR_Device::LEFT].camera.raw_view.inverted().translation();
+            _eyeView[VR_Device::RIGHT].camera.pos = inv_r.translation();
+            _eyeView[VR_Device::LEFT].camera.pos = inv_l.translation();
 
-            vec3 dir =  _offset.to<3>() * hmdDevice.camera().hmdView()[2].to<3>();
-            _cullingView.camera.dir = _cullingView.camera.pos - dir;
+            _eyeView[VR_Device::LEFT].camera.dir =  -_eyeView[VR_Device::LEFT].camera.raw_view[2].to<3>();
+            _eyeView[VR_Device::RIGHT].camera.dir = -_eyeView[VR_Device::RIGHT].camera.raw_view[2].to<3>();
+            _eyeView[VR_Device::LEFT].camera.dir +=  _eyeView[VR_Device::LEFT].camera.pos;
+            _eyeView[VR_Device::RIGHT].camera.dir += _eyeView[VR_Device::RIGHT].camera.pos;
 
-            _eyeView[VR_Device::LEFT].camera.dir = _eyeView[VR_Device::LEFT].camera.pos + dir;
-            _eyeView[VR_Device::RIGHT].camera.dir = _eyeView[VR_Device::RIGHT].camera.pos + dir;
+            mat4 inv_t = _transform.inverted();
+            _cullingView.camera.pos = inv_t.translation();
+            _cullingView.camera.dir = -_transform[2].to<3>() + _cullingView.camera.pos;
+            _cullingView.camera.up = _transform[1].to<3>();
+        }
+
+        void update(const VRDebugCamera& cam, float ratio)
+        {
+            _eyeView[VR_Device::LEFT].camera.useRawMat = true;
+            _eyeView[VR_Device::RIGHT].camera.useRawMat = true;
+
+            mat4 inv_o = _offset.inverted();
+            _transform = cam.viewMat() * inv_o;
+
+            _eyeView[VR_Device::LEFT].camera.raw_proj = mat4::Projection(110, ratio, 0.02, 300);
+            _eyeView[VR_Device::LEFT].camera.raw_view = cam.eyeView(VR_Device::LEFT) * inv_o;
+            _eyeView[VR_Device::RIGHT].camera.raw_proj = _eyeView[VR_Device::LEFT].camera.raw_proj;
+            _eyeView[VR_Device::RIGHT].camera.raw_view = cam.eyeView(VR_Device::RIGHT) * inv_o;
+
+            mat4 inv_l = _eyeView[VR_Device::LEFT].camera.raw_view.inverted();
+            mat4 inv_r = _eyeView[VR_Device::RIGHT].camera.raw_view.inverted();
+
+            _eyeView[VR_Device::RIGHT].camera.pos = inv_r.translation();
+            _eyeView[VR_Device::LEFT].camera.pos = inv_l.translation();
+
+            _eyeView[VR_Device::LEFT].camera.dir =  -_eyeView[VR_Device::LEFT].camera.raw_view[2].to<3>();
+            _eyeView[VR_Device::RIGHT].camera.dir = -_eyeView[VR_Device::RIGHT].camera.raw_view[2].to<3>();
+            _eyeView[VR_Device::LEFT].camera.dir +=  _eyeView[VR_Device::LEFT].camera.pos;
+            _eyeView[VR_Device::RIGHT].camera.dir += _eyeView[VR_Device::RIGHT].camera.pos;
+
+            mat4 inv_t = _transform.inverted();
+            _cullingView.camera.pos = inv_t.translation();
+            _cullingView.camera.dir = -_transform[2].to<3>() + _cullingView.camera.pos;
+            _cullingView.camera.up = _transform[1].to<3>();
         }
 
         void update(const Camera& cam)
         {
-            _cullingView.camera = cam;
+            throw Exception("update(const Camera&) from HmdSceneView not implemented");
 
-            mat4 proj = mat4::Projection(_renderFov, cam.ratio, cam.clipDist.x(), cam.clipDist.y());
-            mat4 view = mat4::View(_cullingView.camera.pos, _cullingView.camera.dir, _cullingView.camera.up);
-            _eyeView[VR_Device::LEFT].camera.useRawMat = true;
-            _eyeView[VR_Device::RIGHT].camera.useRawMat = true;
-            _eyeView[VR_Device::LEFT].camera.raw_proj = proj;
-            _eyeView[VR_Device::RIGHT].camera.raw_proj = proj;
-            _eyeView[VR_Device::RIGHT].camera.raw_view = mat4::Translation(vec3(0.04,0,0))*view;
-            _eyeView[VR_Device::LEFT].camera.raw_view = mat4::Translation(vec3(-0.04,0,0))*view;
+//            _cullingView.camera = cam;
 
-            _eyeView[VR_Device::RIGHT].camera.pos = _eyeView[VR_Device::RIGHT].camera.raw_view.inverted().translation();
-            _eyeView[VR_Device::LEFT].camera.pos = _eyeView[VR_Device::LEFT].camera.raw_view.inverted().translation();
+//            mat4 proj = mat4::Projection(_renderFov, cam.ratio, cam.clipDist.x(), cam.clipDist.y());
+//            mat4 view = mat4::View(_cullingView.camera.pos, _cullingView.camera.dir, _cullingView.camera.up);
+//            _eyeView[VR_Device::LEFT].camera.useRawMat = true;
+//            _eyeView[VR_Device::RIGHT].camera.useRawMat = true;
+//            _eyeView[VR_Device::LEFT].camera.raw_proj = proj;
+//            _eyeView[VR_Device::RIGHT].camera.raw_proj = proj;
+//            _eyeView[VR_Device::RIGHT].camera.raw_view = mat4::Translation(vec3(0.04,0,0))*view;
+//            _eyeView[VR_Device::LEFT].camera.raw_view = mat4::Translation(vec3(-0.04,0,0))*view;
 
-            vec3 dir = _cullingView.camera.dir - _cullingView.camera.pos;
+//            _eyeView[VR_Device::RIGHT].camera.pos = _eyeView[VR_Device::RIGHT].camera.raw_view.inverted().translation();
+//            _eyeView[VR_Device::LEFT].camera.pos = _eyeView[VR_Device::LEFT].camera.raw_view.inverted().translation();
 
-            _eyeView[VR_Device::LEFT].camera.dir = _eyeView[VR_Device::LEFT].camera.pos + dir;
-            _eyeView[VR_Device::RIGHT].camera.dir = _eyeView[VR_Device::RIGHT].camera.pos + dir;
+//            vec3 dir = _cullingView.camera.dir - _cullingView.camera.pos;
+//            _transform = view;
+
+//            _eyeView[VR_Device::LEFT].camera.dir = _eyeView[VR_Device::LEFT].camera.pos + dir;
+//            _eyeView[VR_Device::RIGHT].camera.dir = _eyeView[VR_Device::RIGHT].camera.pos + dir;
         }
+
+        const mat4& transform() const { return _transform; }
 
     private:
         interface::View _cullingView;
@@ -91,6 +133,7 @@ namespace tim {
         float _renderFov = 110;
 
         mat4 _offset = mat4::IDENTITY();
+        mat4 _transform = mat4::IDENTITY();
     };
 }
 
---
+++ Source/OpenVR/VRDebugCamera.h
@@ -0,0 +1,74 @@
+#ifndef VRDEBUGCAMERA_H_INCLUDED
+#define VRDEBUGCAMERA_H_INCLUDED
+
+#include "TIM_SDL/SDLInputManager.h"
+#include "core/Camera.h"
+
+#include "MemoryLoggerOn.h"
+namespace tim
+{
+    class VRDebugCamera
+    {
+    public:
+        VRDebugCamera(SDLInputManager* im, vec3 sizeRoom) : _input(im), _sizeRoom(sizeRoom) {}
+
+        void update(float time)
+        {
+            vec3 hvec = _dir.cross(vec3(0,0,1)).normalized();
+            mat3 rotZ = mat3::RotationZ(-time * _input->mouseRel().x() * 0.5);
+            _dir = rotZ * _dir;
+            _dir = mat3::AxisRotation(hvec, vec3(0,0,1), -time * _input->mouseRel().y() * 0.5) * _dir;
+            _dir.normalize();
+
+            float boostv = 1;
+            if(_input->keyState(SDLK_w).pressed)
+            {
+                _pos += _dir*time*boostv;
+            }
+            else if(_input->keyState(SDLK_s).pressed)
+            {
+                _pos -= _dir*time*boostv;
+            }
+
+            if(_input->keyState(SDLK_a).pressed)
+            {
+                _pos -= hvec*time*boostv;
+            }
+            else if(_input->keyState(SDLK_d).pressed)
+            {
+                _pos += hvec*time*boostv;
+            }
+
+            _pos.z() += _input->mouseWheel().y() * 5 * time;
+
+            _pos.z() = std::min(_sizeRoom.z(), std::max(0.f, _pos.z()));
+            _pos.x() = std::min(_sizeRoom.x()*0.5f, std::max(-_sizeRoom.x()*0.5f, _pos.x()));
+            _pos.y() = std::min(_sizeRoom.y()*0.5f, std::max(-_sizeRoom.y()*0.5f, _pos.y()));
+
+            _eyeView[0] = mat4::View(_pos + hvec*0.05, _pos + hvec*0.05 + _dir, vec3(0,0,1));
+            _eyeView[1] = mat4::View(_pos - hvec*0.05, _pos - hvec*0.05 + _dir, vec3(0,0,1));
+
+            _view = mat4::View(_pos, _pos + _dir, vec3(0,0,1));
+        }
+
+        const mat4& viewMat() const { return _view; }
+        const mat4& eyeView(int e) const { return _eyeView[e]; }
+
+        vec3 pos() const { return _pos; }
+        vec3 dir() const { return _dir; }
+
+    private:
+        SDLInputManager* _input;
+        vec3 _sizeRoom;
+
+        vec3 _pos = {0,0,1.6}, _dir = {0,1,0};
+
+        mat4 _eyeView[2];
+        mat4 _view;
+
+    };
+
+}
+#include "MemoryLoggerOff.h"
+
+#endif // VRDEBUGCAMERA_H_INCLUDED
