e40ac88b36c40b033fb397c3820fa3d9c0ca0286
Williamallas <jerome.torche@epfl.ch>
2017-02-20 13:31:20 +0100

    added OP template

--- Source/TIMEngine2/bullet/HeightFieldShape.h
+++ Source/TIMEngine2/bullet/HeightFieldShape.h
@@ -9,6 +9,7 @@
 namespace tim
 {
 
+template<int UNSUDED=0>
 static btHeightfieldTerrainShape* createHeightFieldShape(vec3 size, const ImageAlgorithm<float>& img)
 {
     size /= vec3(img.size().x()-1, img.size().y()-1,1);
