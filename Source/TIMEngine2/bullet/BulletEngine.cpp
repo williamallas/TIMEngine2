79244d3f6aabb6c79470af75491447ffe6146973
Williamallas <jerome.torche@epfl.ch>
2017-02-20 13:24:08 +0100

    move to cpp

--- Source/TIMEngine2/bullet/BulletEngine.cpp
+++ Source/TIMEngine2/bullet/BulletEngine.cpp
@@ -3,5 +3,58 @@
 namespace tim
 {
 
+BulletEngine::BulletEngine()
+{
+    createWorld(0);
+    gContactBreakingThreshold = 0.005;
+    //dynamicsWorld->getDispatchInfo().m_allowedCcdPenetration=0.0001f;
+}
+
+BulletEngine::~BulletEngine()
+{
+    for(int i=0 ; i<NB_WORLD ; ++i)
+    {
+        delete dynamicsWorld[i];
+        delete solver[i];
+        delete dispatcher[i];
+        delete collisionConfiguration[i];
+        delete broadphase[i];
+    }
+}
+
+void BulletEngine::addObject(BulletObject* o, int worldId)
+{
+    dynamicsWorld[worldId]->addRigidBody(o->body());
+    o->body()->setDamping(0.3, 0.5);
+    o->_world = dynamicsWorld[worldId];
+    o->_indexWorld = worldId;
+}
+
+void BulletEngine::addObject(BulletObject* o, int worldId, int colMask, int colWithMask)
+{
+    dynamicsWorld[worldId]->addRigidBody(o->body(), colMask, colWithMask);
+    o->body()->setDamping(0.3, 0.5);
+    o->_world = dynamicsWorld[worldId];
+    o->_indexWorld = worldId;
+    o->_collisionMask = colMask;
+    o->_collisionWithMask = colWithMask;
+}
+
+void BulletEngine::createWorld(int index)
+{
+    if(dynamicsWorld[index] == nullptr)
+    {
+        broadphase[index] = new btDbvtBroadphase;
+        collisionConfiguration[index] = new btDefaultCollisionConfiguration;
+        dispatcher[index] = new btCollisionDispatcher(collisionConfiguration[index]);
+        solver[index] = new btSequentialImpulseConstraintSolver;
+
+        dynamicsWorld[index] = new btDiscreteDynamicsWorld(dispatcher[index], broadphase[index],
+                                                           solver[index], collisionConfiguration[index]);
+        dynamicsWorld[index]->setGravity(btVector3(0, 0, -9.87));
+        dynamicsWorld[index]->setInternalTickCallback(tickCallback, static_cast<void *>(this), true);
+        //dynamicsWorld[index]->stepSimulation(0.00001, 1);
+    }
+}
 
 }
--- Source/TIMEngine2/bullet/BulletEngine.h
+++ Source/TIMEngine2/bullet/BulletEngine.h
@@ -35,84 +35,16 @@ namespace tim
             }
         }
 
-        BulletEngine()
-        {
-            createWorld(0);
-			gContactBreakingThreshold = 0.005;
-            //dynamicsWorld->getDispatchInfo().m_allowedCcdPenetration=0.0001f;
-        }
+        BulletEngine();
+        ~BulletEngine();
 
-        ~BulletEngine()
-        {
-            for(int i=0 ; i<NB_WORLD ; ++i)
-            {
-                delete dynamicsWorld[i];
-                delete solver[i];
-                delete dispatcher[i];
-                delete collisionConfiguration[i];
-                delete broadphase[i];
-            }
-        }
-
-        void addObject(BulletObject* o, int worldId = 0)
-        {
-            dynamicsWorld[worldId]->addRigidBody(o->body());
-            o->body()->setDamping(0.3, 0.5);
-            o->_world = dynamicsWorld[worldId];
-            o->_indexWorld = worldId;
-        }
-
-        void addObject(BulletObject* o, int worldId, int colMask, int colWithMask)
-        {
-            dynamicsWorld[worldId]->addRigidBody(o->body(), colMask, colWithMask);
-            o->body()->setDamping(0.3, 0.5);
-            o->_world = dynamicsWorld[worldId];
-            o->_indexWorld = worldId;
-            o->_collisionMask = colMask;
-            o->_collisionWithMask = colWithMask;
-        }
-
-        void reinstance(BulletObject* o, int colMask, int colWithMask)
-        {
-            auto* mt = o->body()->getMotionState();
-            o->setMotionState(nullptr);
-
-            BulletObject* b = new BulletObject(mt, o->body()->getCollisionShape(), 1.f / o->body()->getInvMass());
-            b->body()->setFriction(o->body()->getFriction());
-            b->body()->setRestitution(o->body()->getRestitution());
-            b->body()->setRollingFriction(o->body()->getRollingFriction());
-            b->body()->setCcdMotionThreshold(o->body()->getCcdMotionThreshold());
-            b->body()->setCcdSweptSphereRadius(o->body()->getCcdSweptSphereRadius());
-
-            b->body()->setLinearVelocity(o->body()->getLinearFactor());
-            b->body()->setAngularVelocity(o->body()->getAngularVelocity());
-            b->body()->setUserIndex(o->body()->getUserIndex());
-
-
-            addObject(b, o->_indexWorld, colMask, colWithMask);
-
-            o->swap(*b);
-            delete b;
-        }
-
-        void createWorld(int index)
-        {
-            if(dynamicsWorld[index] == nullptr)
-            {
-                broadphase[index] = new btDbvtBroadphase;
-                collisionConfiguration[index] = new btDefaultCollisionConfiguration;
-                dispatcher[index] = new btCollisionDispatcher(collisionConfiguration[index]);
-                solver[index] = new btSequentialImpulseConstraintSolver;
-
-                dynamicsWorld[index] = new btDiscreteDynamicsWorld(dispatcher[index], broadphase[index],
-                                                                   solver[index], collisionConfiguration[index]);
-                dynamicsWorld[index]->setGravity(btVector3(0, 0, -9.87));
-                dynamicsWorld[index]->setInternalTickCallback(tickCallback, static_cast<void *>(this), true);
-                //dynamicsWorld[index]->stepSimulation(0.00001, 1);
-            }
-        }
-
+        void addObject(BulletObject* o, int worldId = 0);
+        void addObject(BulletObject* o, int worldId, int colMask, int colWithMask);
         void removeObject(BulletObject* o) { o->_world->removeRigidBody(o->body()); }
+
+        void createWorld(int index);
+
+        void setGravity(int index, vec3 gravity) { dynamicsWorld[index]->setGravity(btVector3(gravity.x(), gravity.y(), gravity.z())); }
 
         void addInnerPhysicTask(const TickCallback&, int);
 
